b9c585bfd7983f34d24ed12d0c241e34875d5e51
Joel Eliason <joel.eliason119@gmail.com>
2021-12-28 14:59:36 -0700

    Pass check before install.

--- R/io.R
+++ R/io.R
@@ -1,16 +1,27 @@
-create_ppp = function(df,cell_types=c('Tregs','CD8+ T cells')) {
-  Xmin = min(df$X) - 5
-  Xmax = max(df$X) + 5
-  Ymin = min(df$Y) - 5
-  Ymax = max(df$Y) + 5
+#' Create point pattern with certain cell types
+#'
+#' @param X X coordinates of cells
+#' @param Y Y coordinates of cells
+#' @param cell_types string of cell type
+#' @param keep_types types to retain in ppp
+#'
+#' @return spatstat ppp object 
+#' @export
+create_ppp = function(X,Y,cell_types,keep_types=c('Tregs','CD8+ T cells')) {
+  Xmin = min(X) - 5
+  Xmax = max(X) + 5
+  Ymin = min(Y) - 5
+  Ymax = max(Y) + 5
+  
+  df = data.frame(X=X,Y=Y,cell_types=cell_types)
   
   filt = df %>%
-    filter(ClusterName %in% cell_types) %>%
-    mutate(ClusterName = factor(ClusterName))
+    dplyr::filter(cell_types %in% keep_types) %>%
+    dplyr::mutate(cell_types = factor(cell_types))
   
-  pat = ppp(filt$X,filt$Y,c(Xmin,Xmax),c(Ymin,Ymax))
+  pat = spatstat.geom::ppp(filt$X,filt$Y,c(Xmin,Xmax),c(Ymin,Ymax))
   
-  marks(pat) <- filt$ClusterName
+  spatstat.geom::marks(pat) <- filt$cell_types
   
   return(pat)
 }
